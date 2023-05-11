dd3917695963480117c3b0ef9c705a52dc418753
root <root@vsred103.msr.co.jp>
2023-05-11 19:26:15 +0900

    []

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-// mod server;
-
-// fn main() {
-//     server::main();
-// }
-
 use hyper::Client;
 use hyper_tls::HttpsConnector;
 use quick_xml::de::from_str;
 use serde::{Deserialize, Serialize};
 use mongodb::{options::ClientOptions, Client as MongoClient, bson::doc};
-use std::clone::Clone;
+use std::{clone::Clone};
 use serde_json::{Result};
 use std::io::Read;
 use bzip2::read::BzDecoder;
+
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
+use time::{OffsetDateTime, macros::offset, format_description};
+use std::collections::HashMap;
+
 
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 struct OvalRhel {
@@ -30,7 +30,7 @@ struct Definition {
     id: Option<String>,
     #[serde(rename = "@class")]
     class: Option<String>,
-    metadata: Metadata,
+    metadata: Option<Metadata>,
     criteria: Option<Criteria>
 }
 
@@ -39,8 +39,8 @@ struct Metadata {
     title: Option<String>,
     affected: Option<Vec<Affected>>,
     reference: Option<Vec<Reference>>,
-    description: String,
-    advisory: Advisory,
+    description: Option<String>,
+    advisory: Option<Vec<Advisory>>,
 }
 
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
@@ -73,7 +73,7 @@ struct Advisory{
     rights: Option<String>,
     issued: Option<Issued>,
     updated: Option<Updated>,
-    cves: Option<Cves>,
+    cve: Option<Vec<Cve>>,
     bugzilla: Option<Vec<Bugzilla>>,
     affected_cpe_list: Option<Vec<AffectedCpeList>>,
     affected: Option<String>
@@ -92,7 +92,7 @@ struct Updated {
 }
 
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
-struct Cves {
+struct Cve {
     #[serde(rename = "@cvss2")]
     cvss2: Option<String>,
     #[serde(rename = "@cvss3")]
@@ -157,143 +157,9 @@ struct Criterion2 {
     test_ref: Option<String>
 }
 
-//
-
-use time::{OffsetDateTime, macros::offset, format_description};
-
-#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
-struct Root {
-    family: String,
-    osversion: String,
-    definitions: Vec<Definitions>,
-    timestamp: String
-}
-
-
-//
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    // timestamp
-    let utc = OffsetDateTime::now_utc();
-    let jct = utc.to_offset(offset!(+9));
-    let format = format_description::parse(
-        "[year]-[month]-[day] [hour]:[minute]:[second]"
-    ).unwrap();
-    let time = jct.format(&format).unwrap();
-
-    // https client
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
-    for v in 6..10 {
-        let v: &str = &v.to_string();
-        let url = String::from("https://access.redhat.com/security/data/oval/v2/RHEL") + v + "/rhel-" + v + ".oval.xml.bz2";
-        let res = client.get(url.parse().unwrap()).await.unwrap();
-        let resp = hyper::body::to_bytes(res.into_body()).await.unwrap();
-
-        let mut gz = BzDecoder::new(&resp[..]);
-        let mut resp_body = String::new();
-        gz.read_to_string(&mut resp_body).unwrap();
-
-        let oval_rhel: OvalRhel = from_str(&resp_body).unwrap();
-
-        for i in 0..oval_rhel.definitions.definition.len() {
-            let description = &oval_rhel.definitions.definition[i].metadata.description;
-            if description.find("** REJECT **").is_some() {
-                continue;
-            }
-
-        //     let a = &oval_rhel.definitions.definition[i].metadata.advisory.cves;
-
-        //     let cvss2 = &oval_rhel.definitions.definition[i].metadata.advisory.cves.cvss2;
-        //     println!("{:?}", cvss2);
-        //     let cvss3 = &oval_rhel.definitions.definition[i].metadata.advisory.cves.cvss3;
-        //     println!("{:?}", cvss3);
-        //     let cwe = &oval_rhel.definitions.definition[i].metadata.advisory.cves.cwe;
-        //     println!("{:?}", cwe);
-        //     let href = &oval_rhel.definitions.definition[i].metadata.advisory.cves.href;
-        //     println!("{:?}", href);
-        //     let impact = &oval_rhel.definitions.definition[i].metadata.advisory.cves.impact;
-        //     println!("{:?}", impact);
-        //     let public = &oval_rhel.definitions.definition[i].metadata.advisory.cves.public;
-        //     println!("{:?}", public);
-        //     let cve = &oval_rhel.definitions.definition[i].metadata.advisory.cves.cve;
-        //     println!("{:?}", cve);
-        }
-
-
-
-
-                // let cves = vec![];
-                // for c in d.Advisory.Cves {
-                //     cves.push(
-                //         c.CveID,
-                //         c.Cvss2,
-                //         c.cvss3,
-                //         c.cwe,
-                //         c.impact,
-                //         c.href,
-                //         c.public
-                //     )
-                // }
-
-                // let ref = vec![];
-                // for r in d.References {
-                //     ref.push(
-                //         r.Source,
-                //         r.RefID,
-                //         r.RefURL
-                //     )
-                // }
-
-                // let cpe = vec![];
-                // for p in d.Advisory.AffectedCPEList {
-                //     cpe.push(p)
-                // }
-
-                // let bs = vec![];
-                // for b in d.Advisory.Bugzillas {
-                //     bs.push(
-                //         b.ID,
-                //         b.URL,
-                //         b.Title
-                //     )
-                // }
-
-                // let issued = d.Advisory.Issued.Date;
-                // let updated = d.Advisory.Updated.Date;
-
-                // let def = OVALREHL {
-                //     DefinitionID: d.ID,
-                //     Title: d.Title,
-                //     Description: d.Description,
-                //     Advisory: Advisory {
-                //         Severity: d.Advisory.Severity,
-                //         Cves: cves,
-                //         Bugzilla: bs,
-                //         AffectedCPEList: cpe,
-                //         Issued: issued,
-                //         Updated: updated
-                //     },
-                //     Debian: nil,
-                //     AffectedPacks: pkg,
-                //     References: rs
-            //     };
-            // }
-
-        // let root = Root {
-        //     family: String::from("RHEL"),
-        //     osversion: String::from(v),
-        //     definitions: def,
-        //     timestamp: time.clone()
-        // };
-
-        // println!("{:?}", root);
-    }
-
-    
-
     // let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
     // client_options.app_name = Some("My App".to_string());
 
@@ -309,38 +175,142 @@ async fn main() -> Result<()> {
     //     println!("list Collection: {}", collection_name);
     // }
 
-    // let https = HttpsConnector::new();
-    // let client = Client::builder().build::<_, hyper::Body>(https);
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
 
-    // let mut rhel_ver = vec![];
-    // for i in 6..10 {
-    //     rhel_ver.push(i);
-    // }
+    let mut rhel_ver = vec![];
+    for i in 6..10 {
+        rhel_ver.push(i);
+    }
 
-    // for v in rhel_ver {
-    //     let v: &str = &v.to_string();
-    //     let url = String::from("https://access.redhat.com/security/data/oval/v2/RHEL") + v + "/rhel-" + v + ".oval.xml.bz2";
-    //     let res = client.get(url.parse().unwrap()).await.unwrap();
-    //     let resp = hyper::body::to_bytes(res.into_body()).await.unwrap();
+    for v in rhel_ver {
+        let v: &str = &v.to_string();
+        let url = String::from("https://access.redhat.com/security/data/oval/v2/RHEL") + v + "/rhel-" + v + ".oval.xml.bz2";
+        let res = client.get(url.parse().unwrap()).await.unwrap();
+        let resp = hyper::body::to_bytes(res.into_body()).await.unwrap();
 
-    //     let mut gz = BzDecoder::new(&resp[..]);
-    //     let mut resp_body = String::new();
-    //     gz.read_to_string(&mut resp_body).unwrap();
+        let mut gz = BzDecoder::new(&resp[..]);
+        let mut resp_body = String::new();
+        gz.read_to_string(&mut resp_body).unwrap();
 
-    //     let oval_rhel: OvalRhel = from_str(&resp_body).unwrap();
+//
+        // (example)
+        //  <hoge>foo</hoge>
+        //  <hoge>  ... Event::Start
+        //  foo     ... Event::Text
+        //  </hoge> ... Event::End
 
-    //     let col = String::from("RHEL") + v;
-    //     let typed_collection = db.collection::<Definition>(&col);
+        #[derive(Debug, Clone)]
+        struct TableStat {
+            index: u32,
+            description: Vec<String>,
+            cve: Vec<Vec<String>>
+        }
+
+        let mut reader = Reader::from_str(&resp_body);
+        let mut buf = Vec::new();
+        let mut count = 0;
+        let mut skip_buf = Vec::new();
+        let mut found_tables = Vec::new();
+        let mut custom_entities: HashMap<String, String> = HashMap::new();
         
-    //     let filter = doc! {};
-    //     let delete_result = typed_collection.delete_many(filter, None).await.unwrap();
-    //     println!("Deleted {} documents, col:{}", delete_result.deleted_count, col);
+        loop {
+            match reader.read_event_into(&mut buf).unwrap() {
+                Event::Start(e) => {
+                    if let b"definition" = e.name().as_ref() {
+                        count += 1;
+                        let mut stats = TableStat {
+                            index: count,
+                            description: vec![],
+                            cve: vec![]
+                        };
+
+                        loop {
+                            skip_buf.clear();
+                            match reader.read_event_into(&mut skip_buf).unwrap() {
+                                Event::Start(e) => match e.name().as_ref() {
+                                    b"description" => {
+                                        let txt = reader
+                                            .read_text(e.name())
+                                            .expect("Cannot decode text value");
+
+                                        stats.description.push(
+                                            txt.to_string()
+                                        );
+                                    }
+                                    b"cve" => {
+                                        let attr = e
+                                            .attributes()
+                                            .map(|a| {
+                                                a.unwrap()
+                                                    .decode_and_unescape_value_with(&reader, |ent| {
+                                                        custom_entities.get(ent).map(|s| s.as_str())
+                                                    })
+                                                    .unwrap()
+                                                    .into_owned()
+                                            })
+                                            .collect::<Vec<_>>();
+
+                                        stats.cve.push(
+                                            attr
+                                        );
+                                    }
+                                    _ => {}
+                                },
+                                Event::End(e) => {
+                                    if e.name().as_ref() == b"definition" {
+                                        found_tables.push(stats);
+                                        break;
+                                    }
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+                Event::Eof => break,
+                _ => {}
+            }
+            buf.clear();
+        }
+
+        println!("{:?}", found_tables);
+//
+
+        let family = String::from("redhat");
+        let osver = String::from(v);
+
+        let utc = OffsetDateTime::now_utc();
+        let jct = utc.to_offset(offset!(+9));
+        let format = format_description::parse(
+            "[year]-[month]-[day] [hour]:[minute]:[second]"
+        ).unwrap();
+
+        let timestamp = jct.format(&format).unwrap();
+
+        // println!("{:?},{:?},{:?}", family, osver, timestamp);
+
+
+
+        // let oval_rhel: OvalRhel = from_str(&resp_body).unwrap();
+        // let a = oval_rhel.definitions.definition.iter();
+
+        // for i in a {
+        //     println!("{:?}", i);
+        // }
+
+        // let col = String::from("RHEL") + v;
+        // let typed_collection = db.collection::<Definition>(&col);
         
-    //     for i in 0..oval_rhel.definitions.definition.len() {
-    //         let insert_result = typed_collection.insert_one(oval_rhel.definitions.definition[i].clone(), None).await.unwrap();
-    //         println!("document ID:{}, col:{}", insert_result.inserted_id, col);
-    //     }
-    // }
+        // let filter = doc! {};
+        // let delete_result = typed_collection.delete_many(filter, None).await.unwrap();
+        // println!("Deleted {} documents, col:{}", delete_result.deleted_count, col);
+        
+        // for i in 0..oval_rhel.definitions.definition.len() {
+        //     let insert_result = typed_collection.insert_one(oval_rhel.definitions.definition[i].clone(), None).await.unwrap();
+        //     println!("document ID:{}, col:{}", insert_result.inserted_id, col);
+        // }
+    }
 
     Ok(())
 }
