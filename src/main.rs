235870098b499f098d8f11519746bdf29a8e1ef5
root <root@vsred103.msr.co.jp>
2023-05-15 17:21:31 +0900

    []

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,9 @@ use std::{vec, path::PathBuf, fs::File, io::BufRead};
 use hyper::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value, Value::Null};
-use std::io::BufReader;
+use std::io::{BufReader, Read, Write};
+use time::{OffsetDateTime, macros::offset, format_description};
 
-
-#[derive(Debug, Clone)]
-struct OvalPkg {
-    pkg: Vec<String>,
-    ver: Vec<String>
-}
 
 #[derive(Deserialize, Serialize, Debug)]
 struct ScanResult {
@@ -37,9 +32,161 @@ struct PkgList {
     pkgarch: String
 }
 
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct VlunsList {
+    detect: Vec<VlunsDetect>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct VlunsDetect {
+    time: String,
+    hostname: String,
+    ip: Vec<String>,
+    os: String,
+    kernel: String,
+    oval: Value
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Vluns {
+    time: String,
+    hostname: String,
+    ip: Vec<String>,
+    os: String,
+    kernel: String,
+    detect: Ovalinfo
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Ovalinfo {
+    #[serde(rename = "@id")]
+    id: Option<String>,
+    #[serde(rename = "@class")]
+    class: Option<String>,
+    metadata: Option<Metadata>,
+    criteria: Option<Criteria>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Metadata {
+    title: Option<String>,
+    affected: Option<Affected>,
+    reference: Option<Vec<Reference>>,
+    description: Option<String>,
+    advisory: Option<Advisory>,
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Affected {
+    #[serde(rename = "@family")]
+    family: Option<String>,
+    platform: Option<Vec<String>>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Reference {
+    #[serde(rename = "@ref_id")]
+    ref_id: Option<String>,
+    #[serde(rename = "@ref_url")]
+    ref_url: Option<String>,
+    #[serde(rename = "@source")]
+    source: Option<String>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Advisory{
+    #[serde(rename = "@from")]
+    from: Option<String>,
+    severity: Option<String>,
+    rights: Option<String>,
+    issued: Option<Issued>,
+    updated: Option<Updated>,
+    cve: Option<Vec<Cve>>,
+    bugzilla: Option<Vec<Bugzilla>>,
+    affected_cpe_list: Option<AffectedCpeList>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Issued {
+    #[serde(rename = "@date")]
+    date: Option<String>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Updated {
+    #[serde(rename = "@date")]
+    date: Option<String>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Cve {
+    #[serde(rename = "@cvss2")]
+    cvss2: Option<String>,
+    #[serde(rename = "@cvss3")]
+    cvss3: Option<String>,
+    #[serde(rename = "@cwe")]
+    cwe: Option<String>,
+    #[serde(rename = "@href")]
+    href: Option<String>,
+    #[serde(rename = "@impact")]
+    impact: Option<String>,
+    #[serde(rename = "@public")]
+    public: Option<String>,
+    #[serde(rename = "$value")]
+    cve: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Bugzilla {
+    #[serde(rename = "@href")]
+    href: Option<String>,
+    #[serde(rename = "@id")]
+    id: Option<String>,
+    #[serde(rename = "$value")]
+    bugzilla: Option<String>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct AffectedCpeList {
+    cpe: Option<Vec<String>>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Criteria {
+    #[serde(rename = "@operator")]
+    operator: Option<String>,
+    criterion: Option<Vec<Criterion>>,
+    criteria: Option<Vec<Criteria2>>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Criterion {
+    #[serde(rename = "@comment")]
+    comment: Option<String>,
+    #[serde(rename = "@test_ref")]
+    test_ref: Option<String>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Criteria2 {
+    #[serde(rename = "@operator")]
+    operator: Option<String>,
+    criterion: Option<Vec<Criterion2>>
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
+struct Criterion2 {
+    #[serde(rename = "@comment")]
+    comment: Option<String>,
+    #[serde(rename = "@test_ref")]
+    test_ref: Option<String>
+}
+
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    println!("start...");
+
     let mut file_vec: Vec<String> = Vec::new();
 
     let path = String::from("./result/");
@@ -57,6 +204,12 @@ async fn main() -> Result<()> {
     }
 
     for f in file_vec {
+        println!("load file: {:?}", f);
+
+        let mut vluns_l = VlunsList {
+            detect: vec![]
+        };
+
         let file = match File::open(&f) {
             Ok(i) => i,
             Err(err) => panic!("File Open ERROR... {:?}", err),
@@ -65,7 +218,7 @@ async fn main() -> Result<()> {
         let buf = BufReader::new(file);
         let scan_r: ScanResult = serde_json::from_reader(buf).unwrap();
 
-        let release = scan_r.os.split_whitespace().collect::<Vec<_>>();
+        let release = &scan_r.os.split_whitespace().collect::<Vec<_>>();
 
         // RockyLinux
         if release[0] == "Rocky" && release[1] == "Linux" && release[2] == "release" {
@@ -82,11 +235,6 @@ async fn main() -> Result<()> {
 
                 let empty_vec: Vec<Value> = Vec::new();
                 let oval_vec = v.as_array().unwrap_or_else(|| &empty_vec);
-
-                let mut ovalpkg = OvalPkg {
-                    pkg: vec![],
-                    ver: vec![]
-                };
 
                 for d in oval_vec {
                     if d[0]["criteria"]["criteria"][0]["criterion"] != Null {
@@ -107,8 +255,49 @@ async fn main() -> Result<()> {
                             if b.len() == 2{
                                 let pkg = b[0].trim();
                                 let ver = b[1].trim();
-                                ovalpkg.pkg.push(pkg.to_string());
-                                ovalpkg.ver.push(ver.to_string());
+
+                                for scan_p in &scan_r.pkg {
+                                    if pkg == scan_p.pkgname {
+                                        let v: Vec<&str> = ver.split(":").collect();
+        
+                                        let mut p = String::from(&scan_p.pkgver);
+                                        p += "-";
+                                        p += &scan_p.pkgrelease;
+                                        
+                                        if v[1] == p {
+                                            //time
+                                            let utc = OffsetDateTime::now_utc();
+                                            let jct = utc.to_offset(offset!(+9));
+                                            let format = format_description::parse(
+                                                "[year]-[month]-[day] [hour]:[minute]:[second]"
+                                            ).unwrap();
+                                            let time = jct.format(&format).unwrap();
+
+                                            //hostname
+                                            let hostname = String::from(&scan_r.hostname).replace("\n", "");
+
+                                            //ip
+                                            let ip = &scan_r.ip;
+
+                                            //os
+                                            let os = String::from(&scan_r.os).replace("\n", "");
+
+                                            //kernel
+                                            let kernel = String::from(&scan_r.kernel).replace("\n", "");
+
+                                            let mut vluns = VlunsDetect {
+                                                time: time,
+                                                hostname: hostname,
+                                                ip: scan_r.ip.clone(),
+                                                os: os,
+                                                kernel: kernel,
+                                                oval: d.clone()
+                                            };
+
+                                            vluns_l.detect.push(vluns);
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
@@ -131,8 +320,49 @@ async fn main() -> Result<()> {
                             if b.len() == 2{
                                 let pkg = b[0].trim();
                                 let ver = b[1].trim();
-                                ovalpkg.pkg.push(pkg.to_string());
-                                ovalpkg.ver.push(ver.to_string());
+
+                                for scan_p in &scan_r.pkg {
+                                    if pkg == scan_p.pkgname {
+                                        let v: Vec<&str> = ver.split(":").collect();
+        
+                                        let mut p = String::from(&scan_p.pkgver);
+                                        p += "-";
+                                        p += &scan_p.pkgrelease;
+                                        
+                                        if v[1] == p {
+                                            //time
+                                            let utc = OffsetDateTime::now_utc();
+                                            let jct = utc.to_offset(offset!(+9));
+                                            let format = format_description::parse(
+                                                "[year]-[month]-[day] [hour]:[minute]:[second]"
+                                            ).unwrap();
+                                            let time = jct.format(&format).unwrap();
+
+                                            //hostname
+                                            let hostname = String::from(&scan_r.hostname).replace("\n", "");
+
+                                            //ip
+                                            let ip = &scan_r.ip;
+
+                                            //os
+                                            let os = String::from(&scan_r.os).replace("\n", "");
+
+                                            //kernel
+                                            let kernel = String::from(&scan_r.kernel).replace("\n", "");
+
+                                            let mut vluns = VlunsDetect {
+                                                time: time,
+                                                hostname: hostname,
+                                                ip: scan_r.ip.clone(),
+                                                os: os,
+                                                kernel: kernel,
+                                                oval: d.clone()
+                                            };
+
+                                            vluns_l.detect.push(vluns);
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
@@ -141,44 +371,29 @@ async fn main() -> Result<()> {
                         println!("code[388]: 定義されていない新しい値が追加されています: {:?}", d[1]);
                     }
                 }
-
-                for oval_p in ovalpkg.pkg {
-                    for scan_p in &scan_r.pkg {
-                        if oval_p == scan_p.pkgname {
-                            for oval_v in &ovalpkg.ver {
-                                let v: Vec<&str> = oval_v.split(":").collect();
-
-                                let mut p = String::from(&scan_p.pkgver);
-                                p += "-";
-                                p += &scan_p.pkgrelease;
-                                
-                                if v[1] == p {
-                                    println!("脆弱性検出: {:?}{:?}", scan_p.pkgname, scan_p.pkgver);
-
-                                    let d_file: Vec<&str> = f.split("/").collect();
-                                    let d_index = d_file.len()-1;
-
-                                    std::fs::create_dir_all("detect").unwrap();
-                                    let filename = String::from(d_file[d_index]);
-                                    let dir = String::from("detect/") + &filename;
-
-                                    println!("{:?}", dir);
-
-                                    // let serialized = serde_json::to_string(&localinfo).unwrap();
-                                    // let mut w = std::fs::OpenOptions::new()
-                                    //     .write(true)
-                                    //     .create(true)
-                                    //     .open(dir).unwrap();
-                                    // w.write_all(serialized.as_bytes());
-
-                                }
-                            }
-                        }
-                    }
-                }
             }
         }
+
+        println!("{:?}", vluns_l);
+
+        let d_file: Vec<&str> = f.split("/").collect();
+        let d_index = d_file.len()-1;
+
+        std::fs::create_dir_all("detect").unwrap();
+        let filename = String::from(d_file[d_index]);
+        let dir = String::from("detect/") + &filename;
+
+        let serialized = serde_json::to_string(&vluns_l).unwrap();
+        let mut w = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(dir).unwrap();
+        w.write_all(serialized.as_bytes());
+
+
+        println!("finished: {:?}", f);
     }
+    println!("finished...");
 
     Ok(())
 }
