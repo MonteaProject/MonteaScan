01366e4665a47f63e50fc833474b420da99ec9a5
root <root@vsred103.msr.co.jp>
2023-05-17 18:47:19 +0900

    []

---
+++ web/app/app/Provider.tsx
@@ -0,0 +1,16 @@
+// Server Component の制限
+//　・状態を持たない useState/Context
+//　・ライフサイクルフックが使えない useEffect
+//　・ブラウザのみ利用可能なAPIが使えない localStorage
+//　・イベントハンドラーが使えない onClick/onChange
+
+// ChakraProviderは内部でuseStateを利用しているため、Client Componentとして扱うために、"use client"を宣言したファイルでラップします。
+
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+
+export default function Provider({ children }:{ children: React.ReactNode }) {
+    return <ChakraProvider>{children}</ChakraProvider>
+}
---
+++ web/app/app/common/components/index.tsx
@@ -0,0 +1,5 @@
+// ChakraUI を利用するたびに、"use client"を宣言する手間を省くため、
+// ChakraUI のコンポーネントをexportして Client Component として使えるようにする
+
+"use client";
+export * from "@chakra-ui/react";
