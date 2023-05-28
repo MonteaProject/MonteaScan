b6ae1325ec9f1582911cb87857a21a9188de0b3e
me6u6 <me6u6y@gmail.com>
2023-05-28 16:36:30 +0000

    []

--- gui/app/app/page.tsx
+++ gui/app/app/page.tsx
@@ -6,7 +6,6 @@ import { Heading, Box } from "./common/components";
 export default async function Home() {
   return (
     <Box>
-        <Heading as='h3' size='lg' mb={4}>脆弱性件数</Heading>
         {/* @ts-expect-error 現状は jsx が Promise を返すと TypeScript が型エラーを報告するが、将来的には解決される */}
         <VulnsList />
     </Box>
