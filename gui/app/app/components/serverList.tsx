6f95a77c0c40503d0aa4843a08eba711d2bf4b5b
root <root@vsred103.msr.co.jp>
2023-05-29 16:52:47 +0900

    []

--- gui/app/app/components/serverList.tsx
+++ gui/app/app/components/serverList.tsx
@@ -18,26 +18,31 @@ export default async function ServerList() {
   const f = await getServerList();
   return (
     <Box>
-      <table className="responsive-table">
-        <thead className="responsive-table__head">
-          <tr className="responsive-table__row">
-            <th className="responsive-table__head__title responsive-table__head__title--hostname">ホスト名</th>
-            <th className="responsive-table__head__title responsive-table__head__title--status">ステータス</th>
-            <th className="responsive-table__head__title responsive-table__head__title--os">OS</th>
-            <th className="responsive-table__head__title responsive-table__head__title--kernel">カーネル</th>
-            <th className="responsive-table__head__title responsive-table__head__title--time">最終スキャン時間</th>
+      <table className="responsive-serverlist-table">
+        <thead className="responsive-serverlist-table__head">
+          <tr className="responsive-serverlist-table__row">
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--hostname">ホスト名</th>
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--impact-1">深刻度:重大</th>
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--impact-2">深刻度:高</th>
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--impact-3">深刻度:中</th>
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--impact-4">深刻度:小</th>
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--os">OS</th>
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--kernel">カーネル</th>
+            <th className="responsive-serverlist-table__head__title responsive-serverlist-table__head__title--time">最終スキャン時間</th>
           </tr>
         </thead>
         {f.map((d) => (
-        <tbody className="responsive-table__body">
-          <NextLink href={`/select/${d.hostname}`}>
-          <tr className="responsive-table__row">
-            <td className="responsive-table__body__text responsive-table__body__text--hostname">{d.hostname.substring(0, 35)}</td>
-            <td className="responsive-table__body__text responsive-table__body__text--status">
-            <span className="status-indicator status-indicator--active"></span>Active</td>
-            <td className="responsive-table__body__text responsive-table__body__text--os">{d.os.substring(0, 35)}</td>
-            <td className="responsive-table__body__text responsive-table__body__text--kernel">{d.kernel.substring(0, 35)}</td>
-            <td className="responsive-table__body__text responsive-table__body__text--time">{d.time}</td>
+        <tbody className="responsive-serverlist-table__body">
+          <NextLink className="responsive-serverlist-table__link" href={`/select/${d.hostname}`}>
+          <tr className="responsive-serverlist-table__row">
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--hostname">{d.hostname.substring(0, 35)}</td>
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--impact-1">5</td>
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--impact-2">5</td>
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--impact-3">5</td>
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--impact-4">5</td>
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--os">{d.os.substring(0, 35)}</td>
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--kernel">{d.kernel.substring(0, 35)}</td>
+            <td className="responsive-serverlist-table__body__text responsive-serverlist-table__body__text--time">{d.time}</td>
           </tr>
           </NextLink>
         </tbody>
