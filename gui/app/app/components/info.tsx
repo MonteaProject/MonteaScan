d6882bf6f34decbed52edce03e9ce8f95d151a8c
root <root@vsred103.msr.co.jp>
2023-05-29 13:39:34 +0900

    []

--- gui/app/app/components/info.tsx
+++ gui/app/app/components/info.tsx
@@ -20,43 +20,40 @@ export default async function Info ({ infoPromises }: { infoPromises: Pkg[] }) {
     const handleClick = () => {
         onOpen()
     }
-
     return (
         <Box>
-            <div className="container">
-                <table className="responsive-info-table">
-                    <thead className="responsive-info-table__head">
-                        <tr className="responsive-info-table__row">
-                            <th className="responsive-info-table__head__title responsive-table__head__title--cve">CVE-ID</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--impact">深刻度</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--update">アップデート有無</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--pkgname">パッケージ名称</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--pkgver">現行バージョン番号</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--pkgrelease">現行リリース番号</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--upver">最新バージョン番号</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--uprelease">最新リリース番号</th>
-                            <th className="responsive-info-table__head__title responsive-table__head__title--pkgarch">アーキテクチャ</th>
-                        </tr>
-                    </thead>
-                    <button onClick={() => handleClick()}>
-                    {info.map((d) => (
-                    <tbody className="responsive-info-table__body">
-                        <tr className="responsive-info-table__row">
-                            <td className="responsive-info-table__body__text responsive-table__body__text--pkgname">CVE-2023-XXXX</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--impact">Critical</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--pkgname">〇</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--pkgname">{d.pkgname.substring(0, 35)}</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--pkgver">{d.pkgver.substring(0, 20)}</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--pkgrelease">{d.pkgrelease.substring(0, 20)}</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--upver">{d.upver.substring(0, 20)}</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--uprelease">{d.uprelease.substring(0, 20)}</td>
-                            <td className="responsive-info-table__body__text responsive-table__body__text--pkgarch">{d.pkgarch.substring(0, 20)}</td>
-                        </tr>
-                    </tbody>
-                    ))}
-                    </button>
-                </table>
-            </div>
+            <table className="responsive-info-table">
+                <thead className="responsive-info-table__head">
+                    <tr className="responsive-info-table__row">
+                        <th className="responsive-info-table__head__title responsive-table__head__title--cve">CVE-ID</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--impact">深刻度</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--update">アップデート有無</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--pkgname">パッケージ名称</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--pkgver">現行バージョン番号</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--pkgrelease">現行リリース番号</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--upver">最新バージョン番号</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--uprelease">最新リリース番号</th>
+                        <th className="responsive-info-table__head__title responsive-table__head__title--pkgarch">アーキテクチャ</th>
+                    </tr>
+                </thead>
+                <button className="responsive-info-table__button" onClick={() => handleClick()}>
+                {info.map((d) => (
+                <tbody className="responsive-info-table__body">
+                    <tr className="responsive-info-table__row">
+                        <td className="responsive-info-table__body__text responsive-table__body__text--pkgname">CVE-2023-XXXX</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--impact">Critical</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--pkgname">〇</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--pkgname">{d.pkgname.substring(0, 35)}</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--pkgver">{d.pkgver.substring(0, 20)}</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--pkgrelease">{d.pkgrelease.substring(0, 20)}</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--upver">{d.upver.substring(0, 20)}</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--uprelease">{d.uprelease.substring(0, 20)}</td>
+                        <td className="responsive-info-table__body__text responsive-table__body__text--pkgarch">{d.pkgarch.substring(0, 20)}</td>
+                    </tr>
+                </tbody>
+                ))}
+                </button>
+            </table>
             <Drawer onClose={onClose} isOpen={isOpen} size="xl">
                 <DrawerOverlay />
                 <DrawerContent>
