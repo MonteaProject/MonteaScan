240780c1be325f7c8d3b36fdadc4140deaf12c3a
me6u6 <me6u6y@gmail.com>
2023-05-31 13:54:19 +0000

    []

--- gui/app/pages/api/postConfig/index.tsx
+++ gui/app/pages/api/postConfig/index.tsx
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { Setting } from "../../../app/types/settingTypes";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     if (req.method === "POST"){
-        console.log("API", req.body);
         let json = JSON.parse(readFileSync("../../src/config/config.json", "utf8")) as Setting;
         json.server.push(req.body);
+        let save = JSON.stringify(json, null, 2);
+        writeFileSync("../../src/config/config.json", save);
 
         return res.status(200);
     } else {
